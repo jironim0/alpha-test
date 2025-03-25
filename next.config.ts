ba64f78ba39a887c32c13186a8c82c524261d962
Garipov Nadir <nadir_garipov@mail.ru>
2025-03-25 23:34:22 +0300

    vercel 1.0 deploy try

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: "/alpha-test",
-  images: {
-    domains: ['i1.sndcdn.com', "is1-ssl.mzstatic.com", "i.scdn.co", "upload.wikimedia.org", "cdns-images.dzcdn.net",],
-    unoptimized: true,    
-  },
 };
 
 export default nextConfig;
