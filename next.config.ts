c589b60709707bdaaa4286a868895143ee318061
Garipov Nadir <nadir_garipov@mail.ru>
2025-03-25 22:36:18 +0300

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: "/alpha-test",
   images: {
     domains: ['i1.sndcdn.com', "is1-ssl.mzstatic.com", "i.scdn.co", "upload.wikimedia.org", "cdns-images.dzcdn.net",],
     unoptimized: true,    
